92b545ca10d21a753eff4a68dd82ad76de0bc508
Zekeriya <zekeriyaerturk@gmail.com>
2020-10-22 22:32:42 +0300

    exercise 5.20-5.22

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -49,7 +49,62 @@ describe("Blog app", function () {
       cy.contains("author").type("test user");
       cy.contains("url").type("www.testing.com");
       cy.contains("create").click();
-      cy.contains("new blog for testing");
+      cy.contains("new blog for testing test user");
+    });
+
+    describe("when blog created", function () {
+      beforeEach(function () {
+        cy.contains("new blog").click();
+        cy.contains("title").type("new blog for testing");
+        cy.contains("author").type("test user");
+        cy.contains("url").type("www.testing.com");
+        cy.contains("create").click();
+      });
+
+      it("a blog can be liked", function () {
+        cy.contains("show").click();
+        cy.contains("like").click();
+        cy.contains("likes 1");
+      });
+
+      it("a blog can be deleted", function () {
+        cy.contains("show").click();
+        cy.contains("remove").click();
+        cy.get("html").should("not.contain", "new blog for testing test user");
+      });
+    });
+
+    describe("when blogs ordered", function () {
+      beforeEach(function () {
+        cy.login({ username: "tester", password: "password" });
+        cy.createBlog({
+          title: "data 1",
+          author: "writer 1",
+          url: "www.data1.com",
+          likes: 25,
+        });
+        cy.createBlog({
+          title: "data 2",
+          author: "writer 2",
+          url: "www.data2.com",
+          likes: 20,
+        });
+        cy.createBlog({
+          title: "data 3",
+          author: "writer 3",
+          url: "www.data3.com",
+          likes: 30,
+        });
+        cy.visit("http://localhost:3000/");
+      });
+
+      it.only("order by likes", function () {
+        cy.get(".show-details").then((res) => {
+          res[0].innerText.includes("writer 3");
+          res[1].innerText.includes("writer 1");
+          res[2].innerText.includes("writer 2");
+        });
+      });
     });
   });
 });
