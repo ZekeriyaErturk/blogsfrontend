19ca4ce6e2e52f8d3ef5bd5450a2a3e9f106a318
Zekeriya <zekeriyaerturk@gmail.com>
2020-10-22 19:19:38 +0300

    exercise 5.18

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -1,11 +1,38 @@
+const { func } = require("prop-types");
+
 describe("Blog app", function () {
   beforeEach(function () {
     cy.request("POST", "http://localhost:3003/api/testing/reset");
+    const user = {
+      username: "tester",
+      user: "test user",
+      password: "password",
+    };
+    cy.request("POST", "http://localhost:3003/api/users", user);
     cy.visit("http://localhost:3000");
   });
-  it("Login form is shown", function () {
+
+  it("login form is shown", function () {
     cy.contains("Username");
     cy.contains("Password");
     cy.contains("Login");
+  });
+
+  describe("testing login functionality", function () {
+    it("succeeds with correct credentials", function () {
+      cy.get("#username").type("tester");
+      cy.get("#password").type("password");
+      cy.contains("Login").click();
+      cy.contains("blogs");
+    });
+
+    it("fails with wrong credentials", function () {
+      cy.get("#username").type("test");
+      cy.get("#password").type("password");
+      cy.contains("Login").click();
+      cy.get(".error")
+        .should("contain", "wrong username or password")
+        .and("have.css", "color", "rgb(255, 0, 0)");
+    });
   });
 });
